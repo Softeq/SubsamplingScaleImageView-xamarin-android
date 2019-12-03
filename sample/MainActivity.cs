f6ec94c785a6faf7aa448bc097044c08616e0ff1
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2019-12-03 15:03:59 +0300

    Add sample & cleanup.

---
+++ sample/MainActivity.cs
@@ -0,0 +1,26 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Com.Davemorrissey.Labs.Subscaleview;
+
+namespace SubsamplingScaleImageViewBinding.Sample
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+
+
+
+            var source = ImageSource.ForResource(Resource.Drawable.swissroad);
+
+            var imageView = FindViewById<SubsamplingScaleImageView>(Resource.Id.imageView);
+            imageView.SetImage(source);
+        }
+    }
+}
+
